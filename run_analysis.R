355704df5c0defc5563eac5eaef04553ee81dfa1
megwood <47403809+megwood@users.noreply.github.com>
2019-03-11 12:00:43 -0400

    sections 4&5 completed

--- run_analysis.R
+++ run_analysis.R
@@ -60,9 +60,9 @@ activity_labels = read.table("activity_labels.txt")
 #rename column one 
 names(activity_labels)= c("activity", "activity_name")
 
-#merge data with activity labels
+#merge data with activity label names
 data_labeled = full_join(data_merge,activity_labels) %>% 
-  select(subject,activity,activity_name, everything())
+  select(subject,activity,activity_name, everything())  #reorder the columns
 
 ##############################################################################
 
@@ -70,8 +70,24 @@ data_labeled = full_join(data_merge,activity_labels) %>%
 
 ##############################################################################
 
+names(data_labeled)<-gsub("^t", "time", names(data_labeled))
+names(data_labeled)<-gsub("^f", "frequency", names(data_labeled))
+names(data_labeled)<-gsub("Acc", "Accelerometer", names(data_labeled))
+names(data_labeled)<-gsub("Gyro", "Gyroscope", names(data_labeled))
+names(data_labeled)<-gsub("Mag", "Magnitude", names(data_labeled))
+names(data_labeled)<-gsub("BodyBody", "Body", names(data_labeled))
+
+colnames(data_labeled)
+
 ##############################################################################
 
-### 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+### 5. Average of each variable for each activity and each subject.
 
 ##############################################################################
+
+data_summary = data_labeled %>% 
+  select(-activity) %>% 
+  group_by(subject, activity_name) %>% 
+  dplyr::summarise_all(funs(mean))
+
+write.table(data_summary, "run_analysis_final.txt", row.names = FALSE)
